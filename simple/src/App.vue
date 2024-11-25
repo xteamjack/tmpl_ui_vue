00154b4f2365279ea3f116df27d5cbe71a6a0817
xteam jack <xteam.jack@gmail.com>
2024-11-25 15:50:19 +0530

    First commit

---
+++ simple/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <h1 class="block font-sans text-5xl font-semibold leading-tight tracking-normal text-inherit antialiased">AppX</h1>
+  <h3>Next Gen App Development Style</h3>
+  <RouterView />
+</template>
+
+
---
+++ simple/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+  </div>
+</template>
